c3f8575cd244b14d3078b9b4d308db4a16fd981c
VARAD001 <87771313+VARAD001@users.noreply.github.com>
2026-07-04 08:07:21 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (19.56%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,3 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        data = {}
-        for i in range(len(nums)):
-            if nums[i] not in data.keys():
-                data[nums[i]] = 1
-            else:
-                data[nums[i]] +=1
-        for j in range(len(nums)):
-            if data[nums[j]] >  (len(nums)/2):
-                return nums[j]
+        return sorted(nums)[len(nums)//2]
