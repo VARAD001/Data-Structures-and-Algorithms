618ce4cbccee308d37a1ada034869f4122940b45
VARAD001 <87771313+VARAD001@users.noreply.github.com>
2026-07-08 06:48:18 +0530

    Time: 8 ms (52.83%) | Memory: 19.2 MB (85.94%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,15 +1,10 @@
 class Solution:
+    def __pal(self,num,left,right):
+        if left > right:
+            return True
+        if num[left] != num[right]:
+            return False
+        return self.__pal(num,left + 1, right-1)
     def isPalindrome(self, x: int) -> bool:
         num = str(x)
-        left = 0
-        right = len(num) - 1
-        while left < right:
-            if num[left] != num[right]:
-                return False
-            left = left + 1
-            right = right - 1
-        return True
-
-
-
-        
+        return self.__pal(num,0,len(num)-1)
