33fcca8dddd2107ae6d984c67bebf7f07c4bd79e
VARAD001 <87771313+VARAD001@users.noreply.github.com>
2026-07-10 22:48:07 +0530

    Time: 1 ms (48.55%) | Memory: 19.1 MB (98.32%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,15 +1,12 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        origin = {}
-        while n != 1:
-            if n not in origin.keys():
-                origin[n] = True
-            else:
-                return False
-            n = str(n)
-            suming = 0
-            for i in n:
-                suming += int(i)**2
-            n = suming
-        return True
+        origin = set()
+        while n != 1 and n not in origin:
+            origin.add(n)
+            total = 0
+            while n>0:
+                n,last_digit = divmod(n,10)
+                total += last_digit**2
+            n = total
+        return n == 1
         
