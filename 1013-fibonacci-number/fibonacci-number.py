1a8a08c88453da6847ba2a3016651348e06347d0
VARAD001 <87771313+VARAD001@users.noreply.github.com>
2026-08-19 18:37:31 +0530

    Time: 38 ms (93.46%) | Memory: 19.3 MB (19.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        return self.fib(n-1) + self.fib(n-2)
+        fib_list = [0,1]
+        for i in range(2,n+1):
+            next_fib = fib_list[i-1]+fib_list[i-2]
+            fib_list.append(next_fib)
+        return fib_list[n]
         
