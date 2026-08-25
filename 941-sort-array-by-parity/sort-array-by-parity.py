f65ccee2b6b64077d9088d891274c86ca79fdbf6
VARAD001 <87771313+VARAD001@users.noreply.github.com>
2026-08-25 23:45:36 +0530

    Time: 3 ms (47.75%) | Memory: 19.7 MB (52.99%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,6 +1,11 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            if nums[i] % 2 == 0:
-                nums.insert(0,nums.pop(i))
+        L = 0
+        R = len(nums) -1 
+        while L < R:
+            while L<R and nums[L] % 2 == 0:
+                L += 1
+            while L<R and nums[R] % 2 != 0:
+                R-=1
+            nums[L],nums[R] = nums[R],nums[L]
         return nums
